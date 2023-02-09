41c50e62c0efce3702e9abf25630952917dbf1ea
straight-tamago <121408851+straight-tamago@users.noreply.github.com>
2023-02-09 20:57:57 +0900

    Add files via upload

---
+++ mmmmm/mmmmmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mmmmmApp.swift
+//  mmmmm
+//
+//  Created by mini on 2023/02/09.
+//
+
+import SwiftUI
+
+@main
+struct mmmmmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
